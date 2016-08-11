750b7edc0660ce275d188b3667732fc53156cfd3
Nathaniel Steers <nathaniel@deep-blue.co.uk>
2016-08-11 18:01:34 +0100

    Updated Ruby version.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #returns the full title on a per-page basis when one is not supplied.
+
+  def full_title(page_title =  '')
+    base_title = "Ruby on Rails Tutorial App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
