936e5b3e44acd5e7ff08280c0c5488b59ac548eb
Nathaniel Steers <nathaniel@deep-blue.co.uk>
2018-02-09 18:12:56 +0000

    implemented advanced login behaviours

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create # creates a new session
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
-  		log_in(user)
-  		redirect_to(user_url(user.id))
+  	@user = User.find_by(email: params[:session][:email].downcase)
+  	if @user && @user.authenticate(params[:session][:password])
+  		log_in(@user)
+      if params[:session][:remember_me] == '1'
+        remember(@user)
+      else
+        forget(@user)
+      end
+  		redirect_to(user_url(@user.id))
   	else
   		flash.now[:danger] = 'Invalid email/password combination'
   		render 'new'
@@ -14,7 +19,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    if logged_in?
+      log_out
+    end
     redirect_to root_url
   end
 end
